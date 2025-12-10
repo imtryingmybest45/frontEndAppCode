aedf669c182fd4fdfae46fa99e10ee8191cef0fd
imtryingmybest45 <sebokapril@gmail.com>
2025-12-10 09:33:41 -0500

    Add hellYeah

---
+++ src/pages/HelloHow.js
@@ -0,0 +1,10 @@
+function HelloHow(props){
+      const { name } = props; // Destructure the prop
+      return (
+        <div>
+            <p style={{ whiteSpace: 'pre-wrap' }}>fdsalkfdlks</p>
+        </div>
+      );
+    }
+
+export default HelloHow;
