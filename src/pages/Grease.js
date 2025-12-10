5c3c6ec4351474b223fd875590c7d6eba3a10506
imtryingmybest45 <sebokapril@gmail.com>
2025-12-10 13:18:06 -0500

    Add hellYeah

---
+++ src/pages/Grease.js
@@ -0,0 +1,10 @@
+function Grease(){
+      return (
+        <div>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Not generally considered a horror movie, but this is even more horrifying… a musical!  I don’t have much else to say about it. As soon as they all started singing I turned it off… don’t need that creepy stuff rolling around in my head at night.
+</p>
+        </div>
+      );
+    }
+
+export default Grease;
