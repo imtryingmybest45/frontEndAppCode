05454f6718d24af79e1502e95b4c64663152eb47
imtryingmybest45 <sebokapril@gmail.com>
2025-12-10 12:19:00 -0500

    Add hellYeah

---
+++ src/pages/Insidious2.js
@@ -0,0 +1,15 @@
+function Insidious2(){
+      return (
+        <div>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release date: 2013
+<br /> 
+ <br />
+Rating: 9/10
+<br /> 
+ <br />
+I’m pretty sure this Insidious was actually scarier than the first one. They incorporated more complex lore and brought in some more terrifying antagonists, and I think that was a great decision for the franchise. A rare sequel that can hold a candle to the original.</p>
+        </div>
+      );
+    }
+
+export default Insidious2;
