0ba0e8259fd4ac95328fa5afa24de0d5e2548fd2
imtryingmybest45 <sebokapril@gmail.com>
2025-12-10 12:58:13 -0500

    Add hellYeah

---
+++ src/pages/Oculus.js
@@ -0,0 +1,9 @@
+function Oculus(){
+      return (
+        <div>
+            <p style={{ whiteSpace: 'pre-wrap' }}>10/10 so good!  One of my favorites</p>
+        </div>
+      );
+    }
+
+export default Oculus;
