521abdec0d121d55f9109b880723ecb15df54880
imtryingmybest45 <sebokapril@gmail.com>
2025-12-10 12:56:55 -0500

    Add hellYeah

---
+++ src/pages/Insidious3.js
@@ -0,0 +1,15 @@
+function Insidious3(){
+      return (
+        <div>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release date: 2015
+<br /> 
+ <br />
+Rating: 6.5/10
+<br /> 
+ <br />
+It was nice that Lynn Shane was alive in this one (we love her, and if you recall, she got killed at the end of the first Insidious). However, I still didn’t think this movie was that great…definitely not as good as the first two. Ok to watch when I’m bored, but unneeded.</p>
+        </div>
+      );
+    }
+
+export default Insidious3;
