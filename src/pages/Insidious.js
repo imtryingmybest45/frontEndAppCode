9a89f0159400a3cf688135f9f2221d894fd3fe0c
imtryingmybest45 <sebokapril@gmail.com>
2025-12-10 11:46:13 -0500

    Add hellYeah

---
+++ src/pages/Insidious.js
@@ -0,0 +1,15 @@
+function Insidious(){
+      return (
+        <div>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release Date: 2011
+<br /> 
+ <br />
+Rating: 9/10
+<br /> 
+ <br />
+This movie was one of the first horror movies I watched, and it terrifies me to this day. It’s no surprise that it’s amazing; James Wan directed it, and everything that man touches is gold. Also featured is staple horror movie actor Patrick Wilson, who played Ed Warren in the Conjuring.</p>
+        </div>
+      );
+    }
+
+export default Insidious;
