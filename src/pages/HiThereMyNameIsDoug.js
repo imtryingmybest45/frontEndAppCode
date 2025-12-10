80fbd2925790dc96af30b5968dfe4a03c5371e45
imtryingmybest45 <sebokapril@gmail.com>
2025-12-10 10:58:32 -0500

    Add hellYeah

---
+++ src/pages/HiThereMyNameIsDoug.js
@@ -0,0 +1,18 @@
+function HiThereMyNameIsDoug(){
+      return (
+        <div>
+            <p style={{ whiteSpace: 'pre-wrap' }}>I have just met you, and I love you.
+<br /> 
+ <br />
+My master gave me this collar. He is a good and smart master, and he gave me this collar so that I may talk
+<br /> 
+ <br />
+SQUIRREL!
+<br /> 
+ <br />
+Hi there</p>
+        </div>
+      );
+    }
+
+export default HiThereMyNameIsDoug;
