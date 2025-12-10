a263e939df87c3758d94eccc72ba477fa7da12b4
imtryingmybest45 <sebokapril@gmail.com>
2025-12-10 10:18:28 -0500

    Add hellYeah

---
+++ src/pages/WowHellow.js
@@ -0,0 +1,9 @@
+function WowHellow(){
+      return (
+        <div>
+            <p style={{ whiteSpace: 'pre-wrap' }}>this will not work</p>
+        </div>
+      );
+    }
+
+export default WowHellow;
