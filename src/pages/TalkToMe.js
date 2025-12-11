cd4a30be1d3fa15b29962b0823fb7131b1c45f1e
imtryingmybest45 <sebokapril@gmail.com>
2025-12-11 18:50:33 -0500

    Add hellYeah

---
+++ src/pages/TalkToMe.js
@@ -0,0 +1,16 @@
+function TalkToMe(){
+      return (
+        <div>
+            <h1>Talk To Me</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release Date: 2024
+<br /> 
+ <br />
+Rating: 6.5/10
+<br /> 
+ <br />
+Another great one by A24, but not as good as the Conjuring 2.</p>
+        </div>
+      );
+    }
+
+export default TalkToMe;
