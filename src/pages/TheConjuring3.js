de5c2f319319c2d2b0db47db055e533bc8a0fa45
imtryingmybest45 <sebokapril@gmail.com>
2025-12-11 02:02:08 -0500

    Add hellYeah

---
+++ src/pages/TheConjuring3.js
@@ -0,0 +1,16 @@
+function TheConjuring3(){
+      return (
+        <div>
+            <h1>The Conjuring 3</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release Date: 2021
+<br /> 
+ <br />
+Rating: 4/10
+<br /> 
+ <br />
+It is truly a travesty that this movie exists...I blame its mediocrity on James Wan's absence in the creative process. It's like a lousy redo of The Excorcism of Emily Rose (2005), where all of the unique history and lore was replaced with cheap special effects. Honestly, one of the worst movies in the Conjuring franchise. I once again direct you to read my review on The Conjuring 2 if you want an idea of what a true horror movie masterpiece is.</p>
+        </div>
+      );
+    }
+
+export default TheConjuring3;
