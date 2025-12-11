52af6fd846ab5a750cd5e786a34ca34d65db184b
imtryingmybest45 <sebokapril@gmail.com>
2025-12-11 00:10:40 -0500

    Add hellYeah

---
+++ src/pages/GetOut.js
@@ -0,0 +1,16 @@
+function GetOut(){
+      return (
+        <div>
+            <h1>Get Out</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release date: 2017
+<br /> 
+ <br />
+Rating: 7.5/10
+<br /> 
+ <br />
+Another incredible masterpiece by Jordan Peele.</p>
+        </div>
+      );
+    }
+
+export default GetOut;
