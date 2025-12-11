d1ae24c5ab06fcea6dabefcca5da4f0785d24e04
imtryingmybest45 <sebokapril@gmail.com>
2025-12-11 07:41:19 -0500

    Add hellYeah

---
+++ src/pages/Weapons.js
@@ -0,0 +1,16 @@
+function Weapons(){
+      return (
+        <div>
+            <h1>Weapons</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release Date: 2025
+<br /> 
+ <br />
+Rating: 6/10
+<br /> 
+ <br />
+This movie was alright…it was a novel concept, but I feel that the scares and the acting weren’t up to par. A friend of mine clocked it very accurately: “It had an interesting concept, but I don’t think I’d watch it again, especially now that I know the twist.” Now, a great movie with infinite rewatchability is the Conjuring 2, of which you should definitely read my review for.</p>
+        </div>
+      );
+    }
+
+export default Weapons;
