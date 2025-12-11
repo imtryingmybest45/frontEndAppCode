6e42d782ca84ea137b963d024a6116dac8e57023
imtryingmybest45 <sebokapril@gmail.com>
2025-12-11 08:40:36 -0500

    Add hellYeah

---
+++ src/pages/BringHerBack.js
@@ -0,0 +1,16 @@
+function BringHerBack(){
+      return (
+        <div>
+            <h1>Bring Her Back</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release Date: 2025
+<br /> 
+ <br />
+Rating: 7/10
+<br /> 
+ <br />
+This was one of the best horror releases of 2025, in my opinion. A24 did a great job with this one, and it was an excellent decision to bring back the actors who starred in their 2024 hit Talk to Me. The premise of the movie was uniquely messed up in a way that makes your skin crawl. It, however, did not make my skin crawl as much as the Conjuring 2, which is the greatest horror movie of all time. Once again, I point you to the Conjuring 2 review on this website.</p>
+        </div>
+      );
+    }
+
+export default BringHerBack;
