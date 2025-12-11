0309cd7dd5e9b855c9a5f17551d3de5580f0348f
imtryingmybest45 <sebokapril@gmail.com>
2025-12-11 00:41:08 -0500

    Add hellYeah

---
+++ src/pages/TheConjuring3.js
@@ -0,0 +1,16 @@
+function TheConjuring3(){
+      return (
+        <div>
+            <h1>The Conjuring 3</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release date: 2021
+<br /> 
+ <br />
+Rating: 4/10
+<br /> 
+ <br />
+Similar to how we don't talk about Bruno, we don't talk about this Conjuring.</p>
+        </div>
+      );
+    }
+
+export default TheConjuring3;
