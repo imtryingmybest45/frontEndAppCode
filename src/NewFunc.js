316d1e49a87fb39c5ef71a4e639d24029b7e9284
unknown <sebokapril@gmail.com>
2025-12-10 23:59:59 -0500

    adding heading stuff

--- src/NewFunc.js
+++ src/NewFunc.js
@@ -1,6 +1,7 @@
 function NewFunc(){
       return (
         <div>
+            <h1>Insert movie name here</h1>
             <p style={{ whiteSpace: 'pre-wrap' }}>Insert shit here</p>
         </div>
       );
