9a73fff0e074c60cd9fac1005d9b81441c475b60
imtryingmybest45 <sebokapril@gmail.com>
2025-12-11 13:28:25 -0500

    Add hellYeah

---
+++ src/pages/TheAutopsyofJaneDoe.js
@@ -0,0 +1,10 @@
+function TheAutopsyofJaneDoe(){
+      return (
+        <div>
+            <h1>The Autopsy of Jane Doe</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>The Autopsy of Jane Doe is a great movie  because it traps you in that morgue right alongside the characters. The father son dynamic gives the whole thing a grounded feel as the mystery around the body keeps getting stranger. The storm outside and the cramped, underground setting make every little sound feel like a threat. It’s one of those horror films that gets under your skin.</p>
+        </div>
+      );
+    }
+
+export default TheAutopsyofJaneDoe;
