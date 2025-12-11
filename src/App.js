e73a1a62817580eab7d1f6177050adde77accc92
unknown <sebokapril@gmail.com>
2025-12-11 07:22:28 -0500

    thiswontwork

--- src/App.js
+++ src/App.js
@@ -5,15 +5,34 @@ import {Routes} from 'react-router-dom';
 import {Route} from 'react-router-dom';
 import {Link} from 'react-router-dom';
 import {useState} from 'react';
+import {useEffect} from 'react';
 import './App.css'
 
 function App(){
   const [ranVar, settfVar] = useState(true);
   const userData = {name: ranVar, age: settfVar};
+  //const pathname = window.location.pathname; // e.g., '/products'
   const homeClick = () => {
     settfVar(true)
     console.log("hello");
   }
+
+  const handlePageLoad = (event) => {
+    console.log(window.location.pathname);
+    if (window.location.pathname == "/" || window.location.pathname == "/*"){
+      console.log("hello");
+      settfVar(true);
+    }
+    else{
+      console.log("goodbye");
+      settfVar(false);
+    }
+  };
+
+  useEffect(() => {
+    handlePageLoad();
+  },[]); // The empty dependency array ensures this runs only once
+
   return (
     <div>
       <nav className="top-left-div">
