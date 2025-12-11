7634e8def1c74ddc5d2067b592c17b2e5a9d5ee4
imtryingmybest45 <sebokapril@gmail.com>
2025-12-10 22:00:35 -0500

    Add hellYeah

---
+++ src/pages/Up.js
@@ -0,0 +1,9 @@
+function Up(){
+      return (
+        <div>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Fantastic, cute movie 10/10</p>
+        </div>
+      );
+    }
+
+export default Up;
