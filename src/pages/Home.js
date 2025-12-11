3a5009ad4f299a4a473a786d01e089ff79a1326f
imtryingmybest45 <sebokapril@gmail.com>
2025-12-11 09:46:40 -0500

    Updated file via Java API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import TheAutopsyofJaneDoe from './TheAutopsyofJaneDoe';
 import BringHerBack from './BringHerBack';
 import Weapons from './Weapons';
 import TheConjuring4 from './TheConjuring4';
@@ -22,6 +23,7 @@ function Home(props){
   { id: 4, text: 'The Conjuring 4', to: '/TheConjuring4' },
   { id: 5, text: 'Weapons', to: '/Weapons' },
   { id: 6, text: 'Bring Her Back', to: '/BringHerBack' },
+  { id: 7, text: 'The Autopsyof Jane Doe', to: '/TheAutopsyofJaneDoe' },
   ];
   return (
     <div>
@@ -35,6 +37,7 @@ function Home(props){
         <Route path="/TheConjuring4" element={<TheConjuring4 name = {stvar}/>} />
         <Route path="/Weapons" element={<Weapons name = {stvar}/>} />
         <Route path="/BringHerBack" element={<BringHerBack name = {stvar}/>} />
+        <Route path="/TheAutopsyofJaneDoe" element={<TheAutopsyofJaneDoe name = {stvar}/>} />
       </Routes>
       <nav>
         <ul style={{listStyleType: 'none',  padding: "0", margin: "0"}}>
