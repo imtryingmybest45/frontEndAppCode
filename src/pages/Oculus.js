e9fe169fa9db1c793f35eadc25b97bc34cac6b16
imtryingmybest45 <sebokapril@gmail.com>
2025-12-11 21:57:50 -0500

    Add hellYeah

---
+++ src/pages/Oculus.js
@@ -0,0 +1,16 @@
+function Oculus(){
+      return (
+        <div>
+            <h1>Oculus</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release Date: 2016
+<br /> 
+ <br />
+Rating: 7/10
+<br /> 
+ <br />
+This movie was very original, but not as good as the Conjuring 2. You should go read that review.</p>
+        </div>
+      );
+    }
+
+export default Oculus;
