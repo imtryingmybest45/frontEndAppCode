de1cfe1dc521ef3b654d9477bf2e786691a48706
imtryingmybest45 <sebokapril@gmail.com>
2025-12-11 21:52:15 -0500

    Add hellYeah

---
+++ src/pages/TheDescent.js
@@ -0,0 +1,19 @@
+function TheDescent(){
+      return (
+        <div>
+            <h1>The Descent</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release Date: 2006
+<br /> 
+ <br />
+Rating: 8/10
+<br /> 
+ <br />
+Fun fact about this movie...none of the main actresses knew what the cave monsters were going to look like, so all their reactions were genuine.
+<br /> 
+ <br />
+Anyways, you should read my review of the Conjuring 2.</p>
+        </div>
+      );
+    }
+
+export default TheDescent;
