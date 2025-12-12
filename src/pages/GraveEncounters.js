5f4ee3f0ee903cfa0715fff1247e194222bfa404
imtryingmybest45 <sebokapril@gmail.com>
2025-12-12 13:42:10 -0500

    Add hellYeah

---
+++ src/pages/GraveEncounters.js
@@ -0,0 +1,19 @@
+function GraveEncounters(){
+      return (
+        <div>
+            <h1>Grave Encounters</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release Date: 2011
+<br /> 
+ <br />
+Rating: 8.5/10
+<br /> 
+ <br />
+This movie was like the scarier American version of Gonjam Haunted Asylum.
+<br /> 
+ <br />
+I liked it a lot.</p>
+        </div>
+      );
+    }
+
+export default GraveEncounters;
