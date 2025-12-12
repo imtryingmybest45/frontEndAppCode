46b467e9102111263a0483263d56612c7f0f5c4c
imtryingmybest45 <sebokapril@gmail.com>
2025-12-11 22:25:05 -0500

    Add hellYeah

---
+++ src/pages/GraveEncounters2.js
@@ -0,0 +1,16 @@
+function GraveEncounters2(){
+      return (
+        <div>
+            <h1>Grave Encounters 2</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release Date: 2012
+<br /> 
+ <br />
+Rating: 600/10
+<br /> 
+ <br />
+This movie makes me so happy because we get to see our old pal Lance Preston again!</p>
+        </div>
+      );
+    }
+
+export default GraveEncounters2;
