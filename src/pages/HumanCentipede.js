1d9a3486cc0aad82c252793dfcfd81851a791d74
imtryingmybest45 <sebokapril@gmail.com>
2025-12-11 22:09:33 -0500

    Add hellYeah

---
+++ src/pages/HumanCentipede.js
@@ -0,0 +1,16 @@
+function HumanCentipede(){
+      return (
+        <div>
+            <h1>Human Centipede</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release Date: 2010
+<br /> 
+ <br />
+Rating: 1000/10
+<br /> 
+ <br />
+This movie was truly a masterpiece, dare I say better than the second conjuring. I direct you to the Southpark episode where they parody this movie; it's actually pretty accurate to the original.</p>
+        </div>
+      );
+    }
+
+export default HumanCentipede;
