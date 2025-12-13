c8fa47a41b6a8ae19bb2a5cc89c6d34dc03aa1a5
imtryingmybest45 <sebokapril@gmail.com>
2025-12-13 13:34:45 -0500

    Add hellYeah

---
+++ src/pages/BringHerBack.js
@@ -0,0 +1,16 @@
+function BringHerBack(){
+      return (
+        <div>
+            <h1>Bring Her Back</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release Date: 2025
+<br /> 
+ <br />
+Rating: 7.5/10
+<br /> 
+ <br />
+Now THIS was a freaky movie.</p>
+        </div>
+      );
+    }
+
+export default BringHerBack;
