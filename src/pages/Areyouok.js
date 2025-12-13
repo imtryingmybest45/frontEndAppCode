e191077ac1f740750da1a37227b19dcece5092ec
imtryingmybest45 <sebokapril@gmail.com>
2025-12-13 15:04:35 -0500

    Add hellYeah

---
+++ src/pages/Areyouok.js
@@ -0,0 +1,10 @@
+function Areyouok(){
+      return (
+        <div>
+            <h1>Are you ok</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Where r i</p>
+        </div>
+      );
+    }
+
+export default Areyouok;
