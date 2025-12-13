40e34ed1f8da00e8bc61f54c9558e8988dcf6aa7
unknown <sebokapril@gmail.com>
2025-12-13 12:18:47 -0500

    new changes again

--- src/pages/SubmissionPage.js
+++ src/pages/SubmissionPage.js
@@ -43,7 +43,7 @@ function SubmissionPage(){
         //e.preventDefault(); // Prevents default form submission behavior (page reload)
         axios.post('https://tryingthisagain-e6f8d0gqfmgsevft.eastus2-01.azurewebsites.net/genericEndpoint123',formData)
         //axios.post('http://localhost:8080/genericEndpoint123',formData)
-        then(response => setMess(response.data))
+        .then(response => setMess(response.data))
         }
     };
 
