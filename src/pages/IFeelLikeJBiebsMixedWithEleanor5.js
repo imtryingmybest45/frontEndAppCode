bfc7a3851a41d920640ac0349a3db9e9deae1159
imtryingmybest45 <sebokapril@gmail.com>
2025-12-13 12:31:07 -0500

    Add hellYeah

---
+++ src/pages/IFeelLikeJBiebsMixedWithEleanor5.js
@@ -0,0 +1,16 @@
+function IFeelLikeJBiebsMixedWithEleanor5(){
+      return (
+        <div>
+            <h1>I Feel Like JBiebs Mixed With Eleanor 5</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>rating: 10/10
+<br /> 
+ <br />
+how are you
+<br /> 
+ <br />
+I'm fine</p>
+        </div>
+      );
+    }
+
+export default IFeelLikeJBiebsMixedWithEleanor5;
