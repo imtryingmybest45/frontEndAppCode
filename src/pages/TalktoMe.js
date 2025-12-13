026ad84c536bef1e034183d8f8afba20dece2682
imtryingmybest45 <sebokapril@gmail.com>
2025-12-13 13:25:16 -0500

    Add hellYeah

---
+++ src/pages/TalktoMe.js
@@ -0,0 +1,16 @@
+function TalktoMe(){
+      return (
+        <div>
+            <h1>Talk to Me</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release Date: 2024
+<br /> 
+ <br />
+Rating: 6.5/10
+<br /> 
+ <br />
+This movie was pretty freaky, but the acting was subpar, in my opinion. Not as good as its successor Bring Her Back.</p>
+        </div>
+      );
+    }
+
+export default TalktoMe;
