4c90a5e36d26f58e837e0ce75c4e284d23e2557c
imtryingmybest45 <sebokapril@gmail.com>
2025-12-13 14:59:46 -0500

    Add hellYeah

---
+++ src/pages/Abc.js
@@ -0,0 +1,10 @@
+function Abc(){
+      return (
+        <div>
+            <h1>Abc</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>fdasfd</p>
+        </div>
+      );
+    }
+
+export default Abc;
