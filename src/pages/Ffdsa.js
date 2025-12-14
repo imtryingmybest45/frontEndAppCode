dc7bcbc157bd615131934a8a43a8c047f8a264a8
imtryingmybest45 <sebokapril@gmail.com>
2025-12-14 17:52:20 -0500

    Add hellYeah

---
+++ src/pages/Ffdsa.js
@@ -0,0 +1,10 @@
+function Ffdsa(){
+      return (
+        <div>
+            <h1>Ffdsa</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>fdafd</p>
+        </div>
+      );
+    }
+
+export default Ffdsa;
