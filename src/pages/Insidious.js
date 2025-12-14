ebea8bb0c95f3ad1ffe43c88105c46d133eabca9
imtryingmybest45 <sebokapril@gmail.com>
2025-12-14 18:36:46 -0500

    Add hellYeah

---
+++ src/pages/Insidious.js
@@ -0,0 +1,16 @@
+function Insidious(){
+      return (
+        <div>
+            <h1>Insidious</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release Date: 2011
+<br /> 
+ <br />
+Rating: 9/10
+<br /> 
+ <br />
+Amazing movie…really wish it were longer.</p>
+        </div>
+      );
+    }
+
+export default Insidious;
