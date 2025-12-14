40871cb4bbc1fa3c6119b6ca9695bb8ba7c36ce0
imtryingmybest45 <sebokapril@gmail.com>
2025-12-14 17:57:44 -0500

    Add hellYeah

---
+++ src/pages/NewMovieNam.js
@@ -0,0 +1,10 @@
+function NewMovieNam(){
+      return (
+        <div>
+            <h1>New Movie Nam</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>moviewReview</p>
+        </div>
+      );
+    }
+
+export default NewMovieNam;
