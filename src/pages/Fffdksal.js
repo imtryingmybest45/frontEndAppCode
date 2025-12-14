9131f5824378064b7bebfe017ef57965e79d6872
imtryingmybest45 <sebokapril@gmail.com>
2025-12-14 17:52:45 -0500

    Add hellYeah

---
+++ src/pages/Fffdksal.js
@@ -0,0 +1,10 @@
+function Fffdksal(){
+      return (
+        <div>
+            <h1>Fffdksal</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>fdafd</p>
+        </div>
+      );
+    }
+
+export default Fffdksal;
