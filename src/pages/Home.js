f3a7bdfa18b924bb6967ab0c78589061e63c57a3
unknown <sebokapril@gmail.com>
2025-12-14 17:34:17 -0500

    howdy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import TheConjuring2 from './TheConjuring2';
 import BringHerBack from './BringHerBack';
-import TalktoMe from './TalktoMe';
 import {Routes} from 'react-router-dom';
 import {Route} from 'react-router-dom';
 import {Link} from 'react-router-dom';
@@ -24,7 +23,6 @@ function Home(props){
     return [
       { id: 1, text: 'The Conjuring 2', to: '/TheConjuring2' },
       { id: 2, text: 'Bring Her Back', to: '/BringHerBack' },
-      { id: 3, text: 'Talk to Me', to: '/TalktoMe' },
     ];
   }, []); // <-- Empty array ensures it's created only once
 
@@ -47,7 +45,6 @@ function Home(props){
       <Routes>
         <Route path="/TheConjuring2" element={<TheConjuring2 name = {stvar}/>} />
         <Route path="/BringHerBack" element={<BringHerBack name = {stvar}/>} />
-        <Route path="/TalktoMe" element={<TalktoMe name = {stvar}/>} />
       </Routes>
       <nav>
         <ul style={{listStyleType: 'none',  padding: "0", margin: "0"}}>
