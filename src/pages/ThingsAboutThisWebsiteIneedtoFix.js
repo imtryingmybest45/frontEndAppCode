5c11799a4e980492f7e8b7f8897ee4297f2a5c6c
imtryingmybest45 <sebokapril@gmail.com>
2025-12-15 00:33:55 -0500

    Add hellYeah

---
+++ src/pages/ThingsAboutThisWebsiteIneedtoFix.js
@@ -0,0 +1,30 @@
+function ThingsAboutThisWebsiteIneedtoFix(){
+      return (
+        <div>
+            <h1>Things About This Website I need to Fix</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>There are many bugs.
+One of them is that this is going to show up as one long line of text,
+Instead of several lines spaced out.
+<br /> 
+ <br />
+Like this.
+<br /> 
+ <br />
+This is what I want.
+<br /> 
+ <br />
+1. One.
+<br /> 
+ <br />
+2. Two.
+<br /> 
+ <br />
+Three. 3
+<br /> 
+ <br />
+four five six</p>
+        </div>
+      );
+    }
+
+export default ThingsAboutThisWebsiteIneedtoFix;
