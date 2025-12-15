af438702c61e9cc4752ab0c61f56dfa13cd06821
imtryingmybest45 <sebokapril@gmail.com>
2025-12-15 00:19:54 -0500

    Add hellYeah

---
+++ src/pages/Insidious.js
@@ -0,0 +1,23 @@
+function Insidious(){
+      return (
+        <div>
+            <h1>Insidious</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release Date: 2011
+<br /> 
+ <br />
+Rating: 8.5/10
+<br /> 
+ <br />
+Outline of general thoughts:
+<br /> 
+ <br />
+1. A very strong debut of the Insidious franchise
+2. I'm happy that Patrick Wilson is in it, he's great
+3. Dalton is an idiot for falling down the attic
+4. I need to get it so that I can put single spaces in my reviews, because when I submit this it's gonna show up as a space between every single number
+5. Making websites is hard.</p>
+        </div>
+      );
+    }
+
+export default Insidious;
