f7a3ecc59e27f6bb9ef8c3bf74c455b0c02edeea
imtryingmybest45 <sebokapril@gmail.com>
2025-12-15 00:19:07 -0500

    Add hellYeah

---
+++ src/pages/Tav.js
@@ -0,0 +1,10 @@
+function Tav(){
+      return (
+        <div>
+            <h1>Tav</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>10/10 would recommend if you like old short men</p>
+        </div>
+      );
+    }
+
+export default Tav;
