3d3ae217c43a5fbca01161d158791f5fef2c8eeb
imtryingmybest45 <sebokapril@gmail.com>
2025-12-15 18:53:46 -0500

    Add hellYeah

---
+++ src/pages/Weapons.js
@@ -0,0 +1,14 @@
+function Weapons(){
+      return (
+        <div>
+            <h1>Weapons</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release Date: 2025<br /> 
+<br /> 
+Rating: 6.5/10<br /> 
+<br /> 
+Review pending, check back later.</p>
+        </div>
+      );
+    }
+
+export default Weapons;
