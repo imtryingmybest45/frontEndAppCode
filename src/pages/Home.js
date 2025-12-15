08383900b8bc4109c12334d79c1f4866f03037ff
imtryingmybest45 <sebokapril@gmail.com>
2025-12-15 00:36:27 -0500

    Updated file via Java API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import ThingsAboutThisWebsiteIneedtoFix from './ThingsAboutThisWebsiteIneedtoFix';
 import TheConjuring2 from './TheConjuring2';
 import {Routes} from 'react-router-dom';
 import {Route} from 'react-router-dom';
@@ -19,6 +20,7 @@ function Home(props){
   const linksData = useMemo(() => {
     return [
       { id: 1, text: 'The Conjuring 2', to: '/TheConjuring2' },
+      { id: 2, text: 'Things About This Website I need to Fix', to: '/ThingsAboutThisWebsiteIneedtoFix' },
     ];
   }, []); // <-- Empty array ensures it's created only once
   const stvar = "hello";
@@ -33,6 +35,7 @@ function Home(props){
       {tfVar&&<p>Click on a movie name to see the review.</p>}
       <Routes>
         <Route path="/TheConjuring2" element={<TheConjuring2 name = {stvar}/>} />
+        <Route path="/ThingsAboutThisWebsiteIneedtoFix" element={<ThingsAboutThisWebsiteIneedtoFix name = {stvar}/>} />
       </Routes>
       <nav>
         <ul style={{listStyleType: 'none',  padding: "0", margin: "0"}}>
