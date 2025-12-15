beb026c8f0da9bad30fa4974afbce26847300812
imtryingmybest45 <sebokapril@gmail.com>
2025-12-14 19:42:16 -0500

    Add hellYeah

---
+++ src/pages/Insidious.js
@@ -0,0 +1,13 @@
+function Insidious(){
+      return (
+        <div>
+            <h1>Insidious</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>This will mot likelily now tow
+<br /> 
+ <br />
+oh will</p>
+        </div>
+      );
+    }
+
+export default Insidious;
