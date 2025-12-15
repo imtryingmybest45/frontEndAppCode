2ea4a82a0537d6fe27b5f8abc5b4e0d7aaa38bc2
imtryingmybest45 <sebokapril@gmail.com>
2025-12-15 12:49:48 -0500

    Add hellYeah

---
+++ src/pages/Insidious.js
@@ -0,0 +1,23 @@
+function Insidious(){
+      return (
+        <div>
+            <h1>Insidious</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release Date: 2011<br /> 
+<br /> 
+Budget (est. 2025 dollars): $2.1 million USD<br /> 
+<br /> 
+Rating: 8.5/10<br /> 
+<br /> 
+Thoughts:<br /> 
+<br /> 
+-I enjoyed the astral projection concept a lot; I thought that was very original.<br /> 
+-The score is INCREDIBLE, 10/10. It's so infamous that the minute you hear it, you know you're watching an Insidious film<br /> 
+-Lovable characters (Specs, Tucker, Elise). Specs and Tucker's antics give an overall dark and disturbing film some brief moments of levity<br /> 
+-It's incredible how they basically filmed the entire movie in a house (well, technically two houses). I have mad respect for the production team for effectively constructing an entire spirit dimension on basically peanuts<br /> 
+<br /> 
+More thoughts to come.</p>
+        </div>
+      );
+    }
+
+export default Insidious;
