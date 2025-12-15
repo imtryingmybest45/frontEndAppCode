ca6904294d70dfaa2a2597b4431ee6ac01ce9fa9
imtryingmybest45 <sebokapril@gmail.com>
2025-12-14 19:33:38 -0500

    Add hellYeah

---
+++ src/pages/Insidious3.js
@@ -0,0 +1,16 @@
+function Insidious3(){
+      return (
+        <div>
+            <h1>Insidious 3</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Here is naothr rieov
+<br /> 
+ <br />
+fore insaiflj
+<br /> 
+ <br />
+afldsajk</p>
+        </div>
+      );
+    }
+
+export default Insidious3;
