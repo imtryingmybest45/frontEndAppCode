6af9a6a743ec517c48d7fb684dad56d30b4f0585
imtryingmybest45 <sebokapril@gmail.com>
2025-12-14 19:52:00 -0500

    Add hellYeah

---
+++ src/pages/IKnowWhatYouDidLastSummer.js
@@ -0,0 +1,13 @@
+function IKnowWhatYouDidLastSummer(){
+      return (
+        <div>
+            <h1>I Know What You Did Last Summer</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Unfornutaley, there is only one. SADDDD
+<br /> 
+ <br />
+hwo sadk :'0)</p>
+        </div>
+      );
+    }
+
+export default IKnowWhatYouDidLastSummer;
