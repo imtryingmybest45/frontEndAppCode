d599fcaab5721738034b734df30ce99cb377440b
imtryingmybest45 <sebokapril@gmail.com>
2025-12-15 18:48:14 -0500

    Add hellYeah

---
+++ src/pages/Insidious2.js
@@ -0,0 +1,14 @@
+function Insidious2(){
+      return (
+        <div>
+            <h1>Insidious 2</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Release Date: 2013<br /> 
+<br /> 
+Rating: 9/10<br /> 
+<br /> 
+Review pending, check back later.</p>
+        </div>
+      );
+    }
+
+export default Insidious2;
