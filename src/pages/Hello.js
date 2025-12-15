4edc21cf1d99ae3710b419b5d00aa66d0ef57a6b
imtryingmybest45 <sebokapril@gmail.com>
2025-12-15 13:34:30 -0500

    Add hellYeah

---
+++ src/pages/Hello.js
@@ -0,0 +1,10 @@
+function Hello(){
+      return (
+        <div>
+            <h1>Hello</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>How are you now</p>
+        </div>
+      );
+    }
+
+export default Hello;
