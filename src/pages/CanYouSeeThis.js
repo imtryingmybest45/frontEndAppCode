6bb2c33fbb260c4c8e27cee648b9e8cc09039457
imtryingmybest45 <sebokapril@gmail.com>
2025-12-14 22:02:37 -0500

    Add hellYeah

---
+++ src/pages/CanYouSeeThis.js
@@ -0,0 +1,10 @@
+function CanYouSeeThis(){
+      return (
+        <div>
+            <h1>Can You See This</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>LalallalalLllLlL</p>
+        </div>
+      );
+    }
+
+export default CanYouSeeThis;
