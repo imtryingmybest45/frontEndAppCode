4f2111b529c40bc13db7635d801a64d3651a2e2d
imtryingmybest45 <sebokapril@gmail.com>
2025-12-14 20:59:30 -0500

    Add hellYeah

---
+++ src/pages/CanYouSeeThisLinkLexi.js
@@ -0,0 +1,13 @@
+function CanYouSeeThisLinkLexi(){
+      return (
+        <div>
+            <h1>Can You See This Link Lexi</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Hello Lexi I am talking to you now, can you see this link????
+<br /> 
+ <br />
+STOP TALKING TO CHRIS AND PAY ATTENTION TO ME</p>
+        </div>
+      );
+    }
+
+export default CanYouSeeThisLinkLexi;
