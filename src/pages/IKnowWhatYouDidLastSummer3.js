59996cd7293c1fe8871b1decabaa53af83abe276
imtryingmybest45 <sebokapril@gmail.com>
2025-12-14 19:46:58 -0500

    Add hellYeah

---
+++ src/pages/IKnowWhatYouDidLastSummer3.js
@@ -0,0 +1,10 @@
+function IKnowWhatYouDidLastSummer3(){
+      return (
+        <div>
+            <h1>I Know What You Did Last Summer 3</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>I wish there was a 3rd one of these</p>
+        </div>
+      );
+    }
+
+export default IKnowWhatYouDidLastSummer3;
