2363dd64486b98101b9922b40cdb1f9907546303
imtryingmybest45 <sebokapril@gmail.com>
2025-12-15 19:11:51 -0500

    Add hellYeah

---
+++ src/pages/CollinIsLame.js
@@ -0,0 +1,10 @@
+function CollinIsLame(){
+      return (
+        <div>
+            <h1>Collin Is Lame</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>10/10 yes he is he is very lame</p>
+        </div>
+      );
+    }
+
+export default CollinIsLame;
