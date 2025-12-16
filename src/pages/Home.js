b85d469e3ec6702cab83d10e1c1141eaeaf6e99f
imtryingmybest45 <sebokapril@gmail.com>
2025-12-15 22:43:24 -0500

    Deleted file via Java API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,3 @@
-import Insidious from './Insidious';
 import TheConjuring2 from './TheConjuring2';
 import {Routes} from 'react-router-dom';
 import {Route} from 'react-router-dom';
@@ -20,7 +19,6 @@ function Home(props){
   const linksData = useMemo(() => {
     return [
       { id: 1, text: 'The Conjuring 2', to: '/TheConjuring2' },
-      { id: 2, text: 'Insidious', to: '/Insidious' },
     ];
   }, []); // <-- Empty array ensures it's created only once
   const stvar = "hello";
@@ -35,7 +33,6 @@ function Home(props){
       {tfVar&&<p>Click on a movie name to see the review.</p>}
       <Routes>
         <Route path="/TheConjuring2" element={<TheConjuring2 name = {stvar}/>} />
-        <Route path="/Insidious" element={<Insidious name = {stvar}/>} />
       </Routes>
       <nav>
         <ul style={{listStyleType: 'none',  padding: "0", margin: "0"}}>
