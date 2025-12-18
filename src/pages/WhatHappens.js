d8b5e83d0ac434c82308487451024cf95c537e76
imtryingmybest45 <sebokapril@gmail.com>
2025-12-18 11:42:46 -0500

    Add hellYeah

---
+++ src/pages/WhatHappens.js
@@ -0,0 +1,10 @@
+function WhatHappens(){
+      return (
+        <div>
+            <h1>What Happens</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>If you commit a review</p>
+        </div>
+      );
+    }
+
+export default WhatHappens;
