51ae069d6ab21eedbe8be9b4e2b4a9570951d1b5
imtryingmybest45 <sebokapril@gmail.com>
2025-12-18 11:53:10 -0500

    Add hellYeah

---
+++ src/pages/IWroteThis.js
@@ -0,0 +1,10 @@
+function IWroteThis(){
+      return (
+        <div>
+            <h1>I Wrote This</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>By patrick star</p>
+        </div>
+      );
+    }
+
+export default IWroteThis;
