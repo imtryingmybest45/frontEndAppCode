86164d393237407b5f5e8e2265174bffd33e81b4
imtryingmybest45 <sebokapril@gmail.com>
2025-12-18 11:43:10 -0500

    Add hellYeah

---
+++ src/pages/OneAfterTheOther.js
@@ -0,0 +1,10 @@
+function OneAfterTheOther(){
+      return (
+        <div>
+            <h1>One After The Other</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>I wonder what happens? Will it work?</p>
+        </div>
+      );
+    }
+
+export default OneAfterTheOther;
