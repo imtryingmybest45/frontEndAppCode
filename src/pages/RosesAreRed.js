6f5d509a134532494f3582a73afc2e431c0cbc91
imtryingmybest45 <sebokapril@gmail.com>
2025-12-18 11:53:25 -0500

    Add hellYeah

---
+++ src/pages/RosesAreRed.js
@@ -0,0 +1,10 @@
+function RosesAreRed(){
+      return (
+        <div>
+            <h1>Roses Are Red</h1>
+            <p style={{ whiteSpace: 'pre-wrap' }}>Violets are blue</p>
+        </div>
+      );
+    }
+
+export default RosesAreRed;
