9b41259fac53701d48998f1d4eccae5bcad05591
imtryingmybest45 <sebokapril@gmail.com>
2025-12-20 21:59:43 -0500

    Updated file via Java API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import InsidiousChapter3 from './InsidiousChapter3';
-import Insidious from './Insidious';
+import InsidiousChapter2 from './InsidiousChapter2';
 import TheConjuring2 from './TheConjuring2';
 import {Routes} from 'react-router-dom';
 import {Route} from 'react-router-dom';
@@ -23,7 +23,7 @@ function Home(props){
   const linksData = useMemo(() => {
     return [
       { id: 1, text: 'The Conjuring 2', to: '/TheConjuring2' },
-      { id: 2, text: 'Insidious', to: '/Insidious' },
+      { id: 2, text: 'InsidiousChapter2 Chapter 2', to: '/Insidious' },
       { id: 3, text: 'Insidious Chapter 3', to: '/InsidiousChapter3' },
     ];
   }, []); // <-- Empty array ensures it's created only once
@@ -39,7 +39,7 @@ function Home(props){
       {tfVar&&<p>Click on a movie name to see the review.</p>}
       <Routes>
         <Route path="/TheConjuring2" element={<TheConjuring2 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
-        <Route path="/Insidious" element={<Insidious name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
+        <Route path="/InsidiousChapter2" element={<InsidiousChapter2 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/InsidiousChapter3" element={<InsidiousChapter3 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
       </Routes>
       <nav>
