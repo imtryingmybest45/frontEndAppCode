4b72e4701501663f833f34ea199f353181d6ef9f
unknown <sebokapril@gmail.com>
2025-12-23 21:42:55 -0500

    hello

---
+++ src/pages/AccoladesPage.js
@@ -0,0 +1,14 @@
+function Accolades(){
+
+  return (
+    <div>
+        <h1>Accolades for April's Horror Corner</h1>
+        <p>"This is the Sistine Chapel of personal websites! ~my dad</p>
+        <p>You really put all the bells and whistles on it, didn't you, April? ~Chloe</p>
+        <p>Hahahaha I love it! ~Miranda</p>
+        <p>*middle finger emoji* ~Jenna</p>
+    </div>
+  );
+}
+
+export default Accolades;
