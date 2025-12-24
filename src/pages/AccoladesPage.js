ca19617bebe08d5d2d46f1a0cc846a5b2c0c4470
unknown <sebokapril@gmail.com>
2025-12-23 21:58:54 -0500

    hello

--- src/pages/AccoladesPage.js
+++ src/pages/AccoladesPage.js
@@ -3,10 +3,10 @@ function Accolades(){
   return (
     <div>
         <h1>Accolades for April's Horror Corner</h1>
-        <p>"This is the Sistine Chapel of personal websites! ~my dad</p>
-        <p>You really put all the bells and whistles on it, didn't you, April? ~Chloe</p>
-        <p>Hahahaha I love it! ~Miranda</p>
-        <p>*middle finger emoji* ~Jenna</p>
+        <p>"This is the Sistine Chapel of personal websites!" &nbsp;~my dad</p>
+        <p>"You really put all the bells and whistles on it, didn't you, April?" &nbsp;~Chloe</p>
+        <p>"Hahahaha I love it!" &nbsp;~Miranda</p>
+        <p>*middle finger emoji* &nbsp;~Jenna</p>
     </div>
   );
 }
