bc993ef501e85106c64034d326ee06240560a616
imtryingmybest45 <aprilsebou@gmail.com>
2026-01-09 12:10:29 -0500

    multifile commit

--- src/pages/BitinList.js
+++ src/pages/BitinList.js
@@ -19,8 +19,8 @@ function usePersistedState(key, defaultValue) {
 function BitinList(props){
 
     const origMovName = props.origMovName;
-    const movName = "Bitin List'";
-    const movVal = "Yep, the bitin' ";
+    const movName = "Bitin List";
+    const movVal = "fdjsaj bitin list";
 
     const [varVar, setVarVar] = useState(true);
     let prevPath = props.name;
