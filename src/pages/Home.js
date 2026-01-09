b7c2121b487c24642a2e85b7f660fda237cd4eae
unknown <sebokapril@gmail.com>
2026-01-09 11:29:58 -0500

    making major fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,3 @@
-import AnnabelleCreation from './AnnabelleCreation';
 import Annabelle from './Annabelle';
 import Terrifier3 from './Terrifier3';
 import InsidiousChapter2 from './InsidiousChapter2';
@@ -28,13 +27,12 @@ function Home(props){
   const linksData = useMemo(() => {
     
     const linksDataUnsorted = [
-      { id: 1, text: 'The Conjuring 2', to: '/TheConjuring2', name: "The Conjuring 2" },
-      { id: 2, text: 'Insidious', to: '/Insidious', name: "Insidious" },
-      { id: 3, text: 'The Descent', to: '/TheDescent', name: "The Descent" },
-      { id: 5, text: 'Insidious Chapter 2', to: '/InsidiousChapter2', name: "Insidious Chapter 2" },
-      { id: 6, text: 'Terrifier 3', to: '/Terrifier3', name: "Terrifier 3" },
-      { id: 7, text: 'Annabelle', to: '/Annabelle', name: "Annabelle" },
-      { id: 8, text: 'Annabelle', to: '/Annabelle', name: "Annabelle: Creation" },
+      { id: 1, text: "The Conjuring 2", to: "/TheConjuring2", name: "The Conjuring 2" },
+      { id: 2, text: "Insidious", to: "/Insidious", name: "Insidious" },
+      { id: 3, text: "The Descent", to: "/TheDescent", name: "The Descent" },
+      { id: 5, text: "Insidious Chapter 2", to: "/InsidiousChapter2", name: "Insidious Chapter 2" },
+      { id: 6, text: "Terrifier 3", to: "/Terrifier3", name: "Terrifier 3" },
+      { id: 7, text: "Annabelle", to: "/Annabelle", name: "Annabelle" },
     ];
   const linksDataSorted = [...linksDataUnsorted].sort((a, b) => {
       return a.name.localeCompare(b.name);
@@ -60,7 +58,6 @@ function Home(props){
         <Route path="/InsidiousChapter2" element={<InsidiousChapter2 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/Terrifier3" element={<Terrifier3 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/Annabelle" element={<Annabelle name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
-        <Route path="/AnnabelleCreation" element={<AnnabelleCreation name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
       </Routes>
       <nav>
         <ul style={{listStyleType: 'none',  padding: "0", margin: "0"}}>
