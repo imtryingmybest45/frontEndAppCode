223dcfd2e874308c33e293b921c58eb9c3c13d96
imtryingmybest45 <aprilsebou@gmail.com>
2026-01-23 23:21:48 -0500

    multifile commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,3 @@
-import Fridaythe13th from './Fridaythe13th';
 import AnnabelleComesHome from './AnnabelleComesHome';
 import AnnabelleCreation from './AnnabelleCreation';
 import Annabelle from './Annabelle';
@@ -38,7 +37,6 @@ function Home(props){
       { id: 7, text: "Annabelle", to: "/Annabelle", name: "Annabelle" },
       { id: 8, text: "Annabelle: Creation", to: "/AnnabelleCreation", name: "Annabelle: Creation" },
       { id: 9, text: "Annabelle Comes Home", to: "/AnnabelleComesHome", name: "Annabelle Comes Home" },
-      { id: 10, text: "Friday the 13th", to: "/Fridaythe13th", name: "Friday the 13th" },
     ];
   const linksDataSorted = [...linksDataUnsorted].sort((a, b) => {
       return a.name.localeCompare(b.name);
@@ -66,7 +64,6 @@ function Home(props){
         <Route path="/Annabelle" element={<Annabelle name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/AnnabelleCreation" element={<AnnabelleCreation name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/AnnabelleComesHome" element={<AnnabelleComesHome name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
-        <Route path="/Fridaythe13th" element={<Fridaythe13th name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
       </Routes>
       <nav>
         <ul style={{listStyleType: 'none',  padding: "0", margin: "0"}}>
