d7efc801cc471a34d556d340e14bfa942cb03fab
imtryingmybest45 <aprilsebou@gmail.com>
2026-02-07 19:20:32 -0500

    multifile commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import Terrifier2 from './Terrifier2';
 import CollinSucksheisReallylame from './CollinSucksheisReallylame';
 import Terrifier from './Terrifier';
 import TalktoMe from './TalktoMe';
@@ -85,6 +86,7 @@ function Home(props){
       { id: 31, text: "Talk to Me", to: "/TalktoMe", name: "Talk to Me", moviePoster: 'https://m.media-amazon.com/images/M/MV5BY2I2NzJmY2YtYTM3Ni00ZGJhLThkZTItODFhMzhlZjZkMDQ5XkEyXkFqcGc@._V1_SX300.jpg', tier: 'C'},
       { id: 32, text: "Terrifier", to: "/Terrifier", name: "Terrifier", moviePoster: 'https://m.media-amazon.com/images/M/MV5BN2M5MzJlYzctNmZhOC00MTFmLWIxZmUtN2I4NzY5MTlmNDdmXkEyXkFqcGc@._V1_SX300.jpg', tier: 'A'},
       { id: 33, text: "Collin Sucks, he is Really lame", to: "/CollinSucksheisReallylame", name: "Collin Sucks, he is Really lame", moviePoster: 'error', tier: 'S'},
+      { id: 34, text: "Terrifier 2", to: "/Terrifier2", name: "Terrifier 2", moviePoster: 'https://m.media-amazon.com/images/M/MV5BYTUyZWM1MDgtMGZiZi00OWZlLTgwNzItZGU3NTkwN2I4OWNiXkEyXkFqcGc@._V1_SX300.jpg', tier: 'A'},
     ];
   const linksDataSorted = [...linksDataUnsorted].sort((a, b) => {
       return a.name.localeCompare(b.name);
@@ -136,6 +138,7 @@ function Home(props){
         <Route path="/TalktoMe" element={<TalktoMe name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/Terrifier" element={<Terrifier name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/CollinSucksheisReallylame" element={<CollinSucksheisReallylame name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
+        <Route path="/Terrifier2" element={<Terrifier2 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
       </Routes>
       <nav>
         <ul style={{listStyleType: 'none',  padding: "0", margin: "0"}}>
