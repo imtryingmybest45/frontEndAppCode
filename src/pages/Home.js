100c032aa23527e360fdfe11c2cc6a0f5725ccdf
imtryingmybest45 <aprilsebou@gmail.com>
2026-02-13 13:30:35 -0700

    multifile commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import Underground2011 from './Underground2011';
 import TheStrangers from './TheStrangers';
 import InsidiousTheRedDoor from './InsidiousTheRedDoor';
 import Apollo18 from './Apollo18';
@@ -234,6 +235,7 @@ function Home(props){
       { id: 111, text: "Apollo 18", to: "/Apollo18", name: "Apollo 18", moviePoster: 'https://m.media-amazon.com/images/M/MV5BMTk5MTk3OTk3OV5BMl5BanBnXkFtZTcwMzg4MzgxNg@@._V1_SX300.jpg', tier: 'C', longReview: 'N',},
       { id: 112, text: "Insidious: The Red Door", to: "/InsidiousTheRedDoor", name: "Insidious: The Red Door", moviePoster: 'https://m.media-amazon.com/images/M/MV5BYWMwMmY2MzUtZDc1Ni00MDlmLThhOWUtYjVlNmQzMGFkMzZmXkEyXkFqcGc@._V1_SX300.jpg', tier: 'C', longReview: 'N',},
       { id: 113, text: "The Strangers", to: "/TheStrangers", name: "The Strangers", moviePoster: 'https://m.media-amazon.com/images/M/MV5BMTkxODAyODMwNV5BMl5BanBnXkFtZTcwNzk5Nzk2MQ@@._V1_SX300.jpg', tier: 'C', longReview: 'N',},
+      { id: 114, text: "Underground (2011)", to: "/Underground2011", name: "Underground (2011)", moviePoster: boo, tier: 'C', longReview: 'N',},
     ];
   const linksDataSorted = [...linksDataUnsorted].sort((a, b) => {
       return a.name.localeCompare(b.name);
@@ -361,6 +363,7 @@ function Home(props){
         <Route path="/Apollo18" element={<Apollo18 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/InsidiousTheRedDoor" element={<InsidiousTheRedDoor name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/TheStrangers" element={<TheStrangers name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
+        <Route path="/Underground2011" element={<Underground2011 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
       </Routes>
       <nav>
         <ul style={{listStyleType: 'none',  padding: "0", margin: "0"}}>
