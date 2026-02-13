3d86ab87e40495c0ff2fdfe8edeb8bff6656feb8
imtryingmybest45 <aprilsebou@gmail.com>
2026-02-13 13:36:37 -0700

    multifile commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import ParanormalActivity from './ParanormalActivity';
 import AQuietPlaceDayOne from './AQuietPlaceDayOne';
 import TheEmptyMan from './TheEmptyMan';
 import TheHauntinginConnecticut from './TheHauntinginConnecticut';
@@ -244,6 +245,7 @@ function Home(props){
       { id: 116, text: "The Haunting in Connecticut", to: "/TheHauntinginConnecticut", name: "The Haunting in Connecticut", moviePoster: 'https://m.media-amazon.com/images/M/MV5BZmE4Mjk4YjQtZTYzOS00OWYxLWEwMTItMzJmZThhNjQ1OWNjXkEyXkFqcGc@._V1_SX300.jpg', tier: 'C', longReview: 'N',},
       { id: 117, text: "The Empty Man", to: "/TheEmptyMan", name: "The Empty Man", moviePoster: 'https://m.media-amazon.com/images/M/MV5BMzI3OWYwNTQtOWM1Yi00Zjc1LWJmOGItN2UzMTNmNmJmZjE3XkEyXkFqcGc@._V1_SX300.jpg', tier: 'C', longReview: 'N',},
       { id: 118, text: "A Quiet Place: Day One", to: "/AQuietPlaceDayOne", name: "A Quiet Place: Day One", moviePoster: 'https://m.media-amazon.com/images/M/MV5BMDdjZTljZWMtMDIwNi00MTA5LTkxZmItNmY0NDA3ZDM0N2M2XkEyXkFqcGc@._V1_SX300.jpg', tier: 'C', longReview: 'N',},
+      { id: 119, text: "Paranormal Activity", to: "/ParanormalActivity", name: "Paranormal Activity", moviePoster: 'https://m.media-amazon.com/images/M/MV5BMjY1NjcxODQ4MV5BMl5BanBnXkFtZTcwMzUxNjM4Mg@@._V1_SX300.jpg', tier: 'C', longReview: 'N',},
     ];
   const linksDataSorted = [...linksDataUnsorted].sort((a, b) => {
       return a.name.localeCompare(b.name);
@@ -376,6 +378,7 @@ function Home(props){
         <Route path="/TheHauntinginConnecticut" element={<TheHauntinginConnecticut name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/TheEmptyMan" element={<TheEmptyMan name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/AQuietPlaceDayOne" element={<AQuietPlaceDayOne name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
+        <Route path="/ParanormalActivity" element={<ParanormalActivity name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
       </Routes>
       <nav>
         <ul style={{listStyleType: 'none',  padding: "0", margin: "0"}}>
