11c08b7f02e434422cfc3dbf04849a8ce62953cb
imtryingmybest45 <aprilsebou@gmail.com>
2026-02-15 08:44:37 -0700

    multifile commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import Terrifier from './Terrifier';
 import Thesis from './Thesis';
 import UnfriendedDarkWeb from './UnfriendedDarkWeb';
 import Unsane from './Unsane';
@@ -160,6 +161,7 @@ function Home(props){
       { id: 69, text: "Unsane", to: "/Unsane", name: "Unsane", moviePoster: 'https://m.media-amazon.com/images/M/MV5BNTYzMDYzMzA1MV5BMl5BanBnXkFtZTgwODUzODg2NDM@._V1_SX300.jpg', tier: 'B', longReview: 'N',},
       { id: 70, text: "Unfriended: Dark Web", to: "/UnfriendedDarkWeb", name: "Unfriended: Dark Web", moviePoster: 'https://m.media-amazon.com/images/M/MV5BNmYxNGZjY2ItOTg5YS00NzM0LWIxZjktMDFmOWM1MGZhOTNkXkEyXkFqcGc@._V1_SX300.jpg', tier: 'B', longReview: 'N',},
       { id: 71, text: "Thesis", to: "/Thesis", name: "Thesis", moviePoster: 'https://m.media-amazon.com/images/M/MV5BNGQwZWU4YTItMjZjYi00ZjNlLWI1ODctMjg2NzBhOGIwODAwXkEyXkFqcGc@._V1_SX300.jpg', tier: 'B', longReview: 'N',},
+      { id: 72, text: "Terrifier", to: "/Terrifier", name: "Terrifier", moviePoster: boo, tier: 'B', longReview: 'N',},
     ];
   const linksDataSorted = [...linksDataUnsorted].sort((a, b) => {
       return a.name.localeCompare(b.name);
@@ -249,6 +251,7 @@ function Home(props){
         <Route path="/Unsane" element={<Unsane name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/UnfriendedDarkWeb" element={<UnfriendedDarkWeb name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/Thesis" element={<Thesis name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
+        <Route path="/Terrifier" element={<Terrifier name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
       </Routes>
       <nav>
         <ul style={{listStyleType: 'none',  padding: "0", margin: "0"}}>
