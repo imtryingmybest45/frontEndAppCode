c156e2aae799ff2cbca96fc483dab1b8de059941
imtryingmybest45 <aprilsebou@gmail.com>
2026-02-15 12:01:03 -0700

    multifile commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import Host from './Host';
 import HappyDeathDay from './HappyDeathDay';
 import ItChapterTwo from './ItChapterTwo';
 import Poltergeist2015 from './Poltergeist2015';
@@ -226,6 +227,7 @@ function Home(props){
       { id: 106, text: "Poltergeist (2015)", to: "/Poltergeist2015", name: "Poltergeist (2015)", moviePoster: 'https://m.media-amazon.com/images/M/MV5BMjI2NDM1Nzc3Ml5BMl5BanBnXkFtZTgwNjcyOTIxNTE@._V1_SX300.jpg', tier: 'C', longReview: 'N',},
       { id: 107, text: "It Chapter Two", to: "/ItChapterTwo", name: "It Chapter Two", moviePoster: 'https://m.media-amazon.com/images/M/MV5BYzUxM2VhOGItZTY2My00OTA4LWI4NjUtMDA5YWJjM2M2NDJjXkEyXkFqcGc@._V1_SX300.jpg', tier: 'C', longReview: 'N',},
       { id: 108, text: "Happy Death Day", to: "/HappyDeathDay", name: "Happy Death Day", moviePoster: 'https://m.media-amazon.com/images/M/MV5BMTAxNGQwMjEtNjdjNy00NmQwLTkwYTEtNGIwZWJjZjU5M2FmXkEyXkFqcGc@._V1_SX300.jpg', tier: 'C', longReview: 'N',},
+      { id: 109, text: "Host", to: "/Host", name: "Host", moviePoster: 'https://m.media-amazon.com/images/M/MV5BNzNmNDY4ODUtOTRhMi00MGI5LWI2NTItYzdlMGM2ZDEwOGZiXkEyXkFqcGc@._V1_SX300.jpg', tier: 'C', longReview: 'N',},
     ];
   const linksDataSorted = [...linksDataUnsorted].sort((a, b) => {
       return a.name.localeCompare(b.name);
@@ -348,6 +350,7 @@ function Home(props){
         <Route path="/Poltergeist2015" element={<Poltergeist2015 name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/ItChapterTwo" element={<ItChapterTwo name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/HappyDeathDay" element={<HappyDeathDay name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
+        <Route path="/Host" element={<Host name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
       </Routes>
       <nav>
         <ul style={{listStyleType: 'none',  padding: "0", margin: "0"}}>
