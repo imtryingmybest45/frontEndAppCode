999b1fb9a9845f5806e92785388d69daf6ed067e
imtryingmybest45 <aprilsebou@gmail.com>
2026-02-18 22:16:41 -0700

    multifile commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import TheNun from './TheNun';
 import Weapons from './Weapons';
 import Hellbender from './Hellbender';
 import X28DaysLater from './X28DaysLater';
@@ -318,6 +319,7 @@ function Home(props){
       { id: 152, text: "28 Days Later", to: "/X28DaysLater", name: "28 Days Later", moviePoster: 'https://m.media-amazon.com/images/M/MV5BM2I4NTI0ZGQtNGQ2ZC00ODIxLWI2N2QtMDBkNzI1NDhjYjE5XkEyXkFqcGc@._V1_SX300.jpg', tier: 'C', longReview: 'N',},
       { id: 153, text: "Hellbender", to: "/Hellbender", name: "Hellbender", moviePoster: 'https://m.media-amazon.com/images/M/MV5BYjczZDY2NzItZDJhZi00MWEwLWI2YWUtMTY2ZTViMDM1NmZiXkEyXkFqcGdeQXVyMTMxNjczOTQx._V1_SX300.jpg', tier: 'C', longReview: 'N',},
       { id: 154, text: "Weapons", to: "/Weapons", name: "Weapons", moviePoster: 'https://m.media-amazon.com/images/M/MV5BNTBhNWJjZWItYzY3NS00M2NkLThmOWYtYTlmNzBmN2UxZWFjXkEyXkFqcGc@._V1_SX300.jpg', tier: 'C', longReview: 'N',},
+      { id: 155, text: "The Nun", to: "/TheNun", name: "The Nun", moviePoster: 'https://m.media-amazon.com/images/M/MV5BMjM3NzQ5NDcxOF5BMl5BanBnXkFtZTgwNzM4MTQ5NTM@._V1_SX300.jpg', tier: 'D', longReview: 'N',},
     ];
   const linksDataSorted = [...linksDataUnsorted].sort((a, b) => {
       return a.name.localeCompare(b.name);
@@ -486,6 +488,7 @@ function Home(props){
         <Route path="/X28DaysLater" element={<X28DaysLater name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/Hellbender" element={<Hellbender name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/Weapons" element={<Weapons name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
+        <Route path="/TheNun" element={<TheNun name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
       </Routes>
       <nav>
         <ul style={{listStyleType: 'none',  padding: "0", margin: "0"}}>
