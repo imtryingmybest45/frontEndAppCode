b6641ef7d36261acc6b6ce3557dddab3e4ebb2ee
imtryingmybest45 <aprilsebou@gmail.com>
2026-02-19 00:10:01 -0700

    multifile commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import TheWomanintheYard from './TheWomanintheYard';
 import TheGirlNextDoor from './TheGirlNextDoor';
 import TheHumanCentipedeFirstSequence from './TheHumanCentipedeFirstSequence';
 import Skinamarink from './Skinamarink';
@@ -348,6 +349,7 @@ function Home(props){
       { id: 167, text: "Skinamarink", to: "/Skinamarink", name: "Skinamarink", moviePoster: 'https://m.media-amazon.com/images/M/MV5BNTY5OWUwZTUtMzUwZi00MmUwLThiMGItNWJkODE2MjY4MTkyXkEyXkFqcGc@._V1_SX300.jpg', tier: 'F', longReview: 'N',},
       { id: 168, text: "The Human Centipede (First Sequence)", to: "/TheHumanCentipedeFirstSequence", name: "The Human Centipede (First Sequence)", moviePoster: 'https://m.media-amazon.com/images/M/MV5BMTY4Nzk3NzYxOF5BMl5BanBnXkFtZTcwODQwNjQzMw@@._V1_SX300.jpg', tier: 'F', longReview: 'N',},
       { id: 169, text: "The Girl Next Door", to: "/TheGirlNextDoor", name: "The Girl Next Door", moviePoster: 'https://m.media-amazon.com/images/M/MV5BMTUwMDcyNjE1MF5BMl5BanBnXkFtZTcwOTA1Njg0MQ@@._V1_SX300.jpg', tier: 'NO', longReview: 'N',},
+      { id: 170, text: "The Woman in the Yard", to: "/TheWomanintheYard", name: "The Woman in the Yard", moviePoster: 'https://m.media-amazon.com/images/M/MV5BNmZjZTA2ZmQtMDhiYS00NTlmLTk2NTctNTZlMzVhZmVjYTYyXkEyXkFqcGc@._V1_SX300.jpg', tier: 'B', longReview: 'N',},
     ];
   const linksDataSorted = [...linksDataUnsorted].sort((a, b) => {
       return a.name.localeCompare(b.name);
@@ -531,6 +533,7 @@ function Home(props){
         <Route path="/Skinamarink" element={<Skinamarink name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/TheHumanCentipedeFirstSequence" element={<TheHumanCentipedeFirstSequence name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
         <Route path="/TheGirlNextDoor" element={<TheGirlNextDoor name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
+        <Route path="/TheWomanintheYard" element={<TheWomanintheYard name = {props.prevPath} age={props.setPrevPath} reinit={props.reinit} origMovName={origMovName}/>} />
       </Routes>
       <nav>
         <ul style={{listStyleType: 'none',  padding: "0", margin: "0"}}>
